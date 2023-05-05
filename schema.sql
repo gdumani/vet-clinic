9484ee872dc42e1606a02cb9e7fae4ebab277a71
Giancarlo Dumani <gdumani@gmail.com>
2023-05-05 17:30:40 -0600

    Update schema.sql create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 /* Database schema to keep the structure of entire database. */
+createdb vet_clinic
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals ( id SERIAL PRIMARY KEY, name VARCHAR(20), date_of_birth DATE, escape_attempts int, neutered BOOL, weight_kg DECIMAL(4,2));
