9f7ddaf384364efbc080c089876c020c83535ec3
Giancarlo Dumani <gdumani@gmail.com>
2023-05-05 22:42:07 -0600

    Update schema.sql to add species on animals table

--- schema.sql
+++ schema.sql
@@ -2,3 +2,5 @@
 createdb vet_clinic
 
 CREATE TABLE animals ( id SERIAL PRIMARY KEY, name VARCHAR(20), date_of_birth DATE, escape_attempts int, neutered BOOL, weight_kg DECIMAL(4,2));
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(20);
