ca0114a797777a5b626980ba687966f12319c142
Giancarlo Dumani <gdumani@gmail.com>
2023-05-05 22:48:34 -0600

    Update queries.sql with transactions

--- queries.sql
+++ queries.sql
@@ -15,3 +15,25 @@ SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022/01/01';
+SAVEPOINT sp;
+UPDATE animals SET weight_kg = weight_kg*-1;
+ROLLBACK TO sp;
+UPDATE animals SET weight_kg = weight_kg*-1 WHERE weight_kg < 0;
+COMMIT;
