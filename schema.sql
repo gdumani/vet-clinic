62389b845fc42503d40fa79b2be46c60627a28ec
Giancarlo Dumani <gdumani@gmail.com>
2023-05-06 15:15:57 -0600

    Update schema.sql create vets, specialization, visits

--- schema.sql
+++ schema.sql
@@ -16,4 +16,19 @@ DROP COLUMN species,
 ADD COLUMN species_id INT REFERENCES species(id),
 ADD COLUMN owner_id INT REFERENCES owners(id);
 
+/*=============================================================*/
 
+CREATE TABLE vets (id SERIAL PRIMARY KEY, name VARCHAR(50), age INT, date_of_graduation DATE);
+
+CREATE TABLE specialization (
+	vet_id INT REFERENCES vets(id), 
+	species_id INT REFERENCES species(id), 
+	PRIMARY KEY (vet_id, species_id)
+);
+
+CREATE TABLE visits (
+	vet_id INT REFERENCES vets(id),
+	animal_id INT REFERENCES animals(id), 
+	visit_date DATE, 
+	PRIMARY KEY (vet_id, animal_id, visit_date)
+); 
